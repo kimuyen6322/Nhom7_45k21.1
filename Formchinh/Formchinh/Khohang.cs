b2bef4e2cc7e185b4162fc1162d6af8d06ec6b4d
Kim Uyên <85609550+kimuyen6322@users.noreply.github.com>
2021-11-06 13:14:37 +0700

    Add files via upload

---
+++ Formchinh/Formchinh/Khohang.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formchinh
+{
+    public partial class Khohang : Form
+    {
+        public Khohang()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
